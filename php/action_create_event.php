e3829df7d3c918437edbe31b0f70edc7ad6daf3b
SofiaReis <sofiareis1994@gmail.com>
2015-12-05 04:23:34 +0000

    Create-event working :)

--- php/action_create_event.php
+++ php/action_create_event.php
@@ -1,25 +1,23 @@
 <?php
-	
+
+	print_r($_POST);	
 	include_once 'db_connection.php';
 	include_once 'db_utilities_event.php';
 
 	session_start();
 
-	/*if (! isset ( $_SESSION ['user_id'] )) {
+	if (! isset ( $_SESSION ['user_id'] )) {
 		echo 'Need to Login.';
 		return false;
-	}*/
+	}
 
 	$count = getEventLastID();
-
-	
 
 	$name = $_FILES["file"]["name"];
 	$type = $_FILES["file"]["type"];
 	$size = $_FILES["file"]["size"];
 
 	$userID = $_SESSION['user_id'];
-	print_r($userID);
 
 	//print_r(basename($_FILES["file"]["name"]));
 
@@ -32,13 +30,27 @@
 		return false;
 	}
 	if (! isset ( $_POST ['description'])) {
-		echo 'Title empty';
+		echo 'Description empty';
 		return false;
 	}
-	if ($_POST ['estado'] == 'public') {
+	if (isset($_POST ['estado'])) {
 		$private = 0;
 	} else {
 		$private = 1;
+	}
+	if (! isset ( $_POST ['type'])) {
+		echo 'Category empty';
+		return false;
+	}
+
+	if (! isset ( $_POST ['date'])) {
+		echo 'Date empty';
+		return false;
+	}
+
+	if (! isset ( $_POST ['local'])) {
+		echo 'Local empty';
+		return false;
 	}
 
 	if(!isset($_FILES["file"])){
@@ -46,19 +58,21 @@
 		return false;
 	}
 	
-
-	echo ' CHEGUEI AQUI';
-	
-
-	
-	
-
-	function new_event(){
-
-		
-	}
+	$stmt = $db->prepare ('INSERT INTO Event (name,description,idImage,idType, date, idUser, public, local) VALUES (?,?,?,?,?,?,?,?)');
+	$stmt->execute(array(htmlentities($_POST['title'],ENT_QUOTES),
+			htmlentities($_POST['description'],ENT_QUOTES),
+			'0',
+			$_POST['type'],
+			htmlentities($_POST['date'],ENT_QUOTES),
+			htmlentities($userID,ENT_QUOTES),
+			htmlentities($private,ENT_QUOTES),
+			htmlentities($_POST['local'],ENT_QUOTES)));
 
 	
+
+
+
+
 
 
 	?>
