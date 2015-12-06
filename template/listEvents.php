b8e10ae860475c1e095b4dda670b2924d5909527
Maria Marques <mariajoao.rmarques@gmail.com>
2015-12-06 17:53:45 +0000

    listEvents done, missing select only public events and maybe icons

--- template/listEvents.php
+++ template/listEvents.php
@@ -20,16 +20,20 @@
 									<h1>
 										<? $date = $events[$i]['date'];
 										list($y, $m, $d) = explode("-", $date);
-										echo $d."/".$m?></h1>
+										?>
+										<div id="day"><? echo $d; ?></div>
+										<div id="month"><? echo date('M', strtotime($date)); ?></div></h1>
 									</div>
+									<div id="t">
 									<h1><? echo $events[$i]['name']; ?></h1>
 									<p><span>created by </span><?
 									$idU = $events[$i]['idUser'];
 									$creator=getCreator($idU);
 									echo $creator['username'];?></p>
+									</div>
 								</div>
 								<div class="info">
-									<? echo $events[$i]['description']; ?><br><? echo $events[$i]['local']; ?><br><?
+									<? echo $events[$i]['description']; ?><hr><? echo $events[$i]['local']; ?><br><?
 									$idT = $events[$i]['idType'];
 									$type=getEventType($idT);
 									echo $type['name'];
@@ -46,5 +50,3 @@
 
 				</div>
 			</section>
-
-
